2127a3158d576b1667e8181874cd9d1974b653c2
Sa-eedah <saeedahmeyer62@gmail.com>
2025-10-02 09:00:37 +0200

    changes to templates

--- shopco/models.py
+++ shopco/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    rating = models.FloatField(default=0.0)
+    image = models.ImageField(upload_to="products/")
+
+    def __str__(self):
+        return self.name
